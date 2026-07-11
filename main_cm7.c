87cd8253506fe67cb8e8325ee15a4d1f580d373c
Alex Jegers <awjpp@outlook.com>
2026-07-11 17:08:50 -0500

    Squashed commit of the following:
    commit db04c6a4297be479c0c213d90e3ee73bcc481b25
    Author: Alex Jegers <awjpp@outlook.com>
    Date:   Sat Jul 11 17:07:30 2026 -0500
    
        Main
    
    commit d595971a5abc919878b5e145bc1684b192b670bf
    Author: Alex Jegers <awjpp@outlook.com>
    Date:   Sat Jul 11 17:06:27 2026 -0500
    
        Update gitignore
    
    commit 62ef172d03929d9be04a59286a61633b3021aac8
    Author: Alex Jegers <awjpp@outlook.com>
    Date:   Sat Jul 11 17:05:19 2026 -0500
    
        rename files
    
    commit 4f0a9591205f9d4cc81c825c4a8c03a5b6423565
    Author: Alex Jegers <awjpp@outlook.com>
    Date:   Sat Jul 11 17:04:29 2026 -0500
    
        create gitignore

--- main_cm7.c
+++ main_cm7.c
@@ -10,7 +10,7 @@
 #define TARGET_HARDWARE_CANGAUGE
 
 #include "cangauge.h"
-#include "demos/benchmark/lv_demo_benchmark.h"
+#include "application/applications_cm7.h"
 
 int main(void)
 {
@@ -29,7 +29,10 @@ int main(void)
    	xTaskCreate(system_task_init, "SYS_INIT", 650, NULL, 5, NULL);
 
 	/* Load the menu screen. */
-	system_set_ui_init_cb(lv_demo_benchmark);
+	system_set_ui_init_cb(app_gauges_run);
+
+	/* Start the battery power monitor. */
+	pwr_monitor_run(3);
 
 	/* Starts the FreeRTOS scheduler. */
 	vTaskStartScheduler();
