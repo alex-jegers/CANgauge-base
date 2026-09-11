150d0aa22b19b8495de1726d2903c2d0446dbf33
Alex Jegers <awjpp@outlook.com>
2026-09-11 15:59:54 -0500

    Update filesys_helpers
    Added function to get the largest contiguous space in a drive.

--- cg/file_system/filesys_helpers.c
+++ cg/file_system/filesys_helpers.c
@@ -1,5 +1,10 @@
 /**********     INCLUDES        **********/
-#include <file_system/filesys_helpers.h>
+#include "file_system/filesys_helpers.h"
+#include "file_system/fatfs/ff.h"
+#include "file_system/fatfs/diskio.h"
+#include <string.h>
+#include <stdlib.h>
+
 /**********     TYPEDEFS         **********/
 
 /**********		DEFINES		**********/
@@ -12,8 +17,9 @@
 
 /**********		STATIC FUNCTION DEFINITIONS		**********/
 
+
 /**********		GLOBAL FUNCTION DEFINITIONS		**********/
-uint32_t filesys_get_free_space(const char* drive)
+uint32_t filesys_get_total_free_space_bytes(const char* drive)
 {
 	DWORD free_clusters = 0;
 	FATFS* fs_ptr;
@@ -24,4 +30,46 @@ uint32_t filesys_get_free_space(const char* drive)
 	disk_ioctl(fs_ptr->pdrv, GET_SECTOR_SIZE, &bytes_per_sector);
 
 	return free_sectors * bytes_per_sector;
+}
+
+uint32_t filesys_get_contiguous_free_space_bytes(const char* drive)
+{
+	/* Get the sector size and total available space. */
+	/* This is basically copy and pasted from filesys_get_total_free_space_bytes
+	 * but were calling it again so we can store the sector size in this function
+	 * cause we need it later.
+	 */
+	DWORD free_clusters = 0;
+	FATFS* fs_ptr;
+	f_getfree((const TCHAR*)drive, &free_clusters, &fs_ptr);
+	uint32_t free_sectors = free_clusters * fs_ptr->csize;
+	uint32_t bytes_per_sector = 0;
+	disk_ioctl(fs_ptr->pdrv, GET_SECTOR_SIZE, &bytes_per_sector);
+	uint32_t space = free_sectors * bytes_per_sector;
+
+	FIL file;
+	FRESULT res;
+	f_chdrive(drive);		//Change the current drive.
+
+	/* Open a temp file and attempt to expand less and less it until we can. */
+	res = f_open(&file, "temp.txt", FA_CREATE_NEW | FA_WRITE);
+	if (res != FR_OK)
+	{
+		return 0;
+	}
+	for (;space > bytes_per_sector; space -= 512)
+	{
+		res = f_expand(&file, space, 0);
+		if (res == FR_OK)
+		{
+			break;
+		}
+	}
+
+	/* Close and delete the temp file. */
+	f_close(&file);
+	f_unlink("temp.txt");
+	f_chdrive("0:/");		//Change the drive back to zero.
+
+	return space - bytes_per_sector;		//Always leave 1 sector open.
 }
--- cg/file_system/filesys_helpers.h
+++ cg/file_system/filesys_helpers.h
@@ -1,4 +1,10 @@
-
+/**
+ * @file filesys_helpers.h
+ * @author Alex Jegers
+ * @brief Contains utility functions that help or enhance the use of the rest of the FatFS API.
+ *          Depends on the FatFS API as well as standard library files string.h, stdlib.h, and
+ *          stdint.h.
+ */
 #ifndef _FILESYS_HELPERS_H_
 #define _FILESYS_HELPERS_H_
 
@@ -7,8 +13,7 @@ extern "C" {
 #endif
 
 /**********     INCLUDES        **********/
-#include "file_system/fatfs/ff.h"
-#include "file_system/fatfs/diskio.h"
+#include <stdint.h>
 /**********     TYPEDEFS         **********/
 
 /**********     DEFINES      **********/
@@ -17,12 +22,16 @@ extern "C" {
 
 /**********		GLOBAL FUNCTION DECLRATIONS		**********/
 /**
- * name:
- *      desc:
- *      params:
- *      returns:
+ * @brief Gets the total amount of space available in a file system.
+ *          This is not necessarily the amount of usable space as it
+ *          may not be continuous. Use filesys_get_contiguous_free_space_bytes
+ *          for the largest amount of continuous space.
+ * @param drive The volume of which to check for the available space of (i.e. "0:/" or "1:/" or "0:").
+ * @returns The total available space in bytes on the drive. Will return 0 on
+ *          file system error as well.
  */
-uint32_t filesys_get_free_space(const char* drive);
+uint32_t filesys_get_total_free_space_bytes(const char* drive);
+uint32_t filesys_get_contiguous_free_space_bytes(const char* drive);
 
 
 
